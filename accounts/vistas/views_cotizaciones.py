55321503c086b5e087b958c30a5d2d9b95d617bb
AranzaGtz <aranza.gutierrezm.ok@gmail.com>
2024-07-11 16:52:42 -0700

    Actualizaciones, formularios

--- accounts/vistas/views_cotizaciones.py
+++ accounts/vistas/views_cotizaciones.py
@@ -3,16 +3,21 @@ from django.db import IntegrityError
 from django.forms import modelformset_factory
 from django.shortcuts import get_object_or_404, render, redirect
 from accounts.models import Cotizacion, Concepto, Empresa, InformacionContacto, Organizacion, Formato, CustomUser, Persona, Prospecto, Servicio, Titulo
-from accounts.forms import ConceptoForm, CotizacionForm, CotizacionChangeForm, ConceptoFormSet, ConceptoChangeFormSet, DireccionForm, EmpresaForm, PersonaForm, ProspectoForm, TerminosForm
+from accounts.forms import ConceptoForm, CotizacionForm, CotizacionChangeForm, ConceptoFormSet, DireccionForm, EmpresaForm, PersonaForm, ProspectoForm, TerminosForm
 from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, get_object_or_404
-# type: ignore #AQUI SALE WARNING: No se ha podido resolver la importación "weasyprint".
 from weasyprint import HTML  # type: ignore
-# Asegúrate de importar render_to_string
 from django.template.loader import render_to_string
 from django.db import IntegrityError, transaction
 
+# VISTA PARA GENERAD ID PERSONALIZADO
+def generate_new_id_personalizado():
+    last_cotizacion = Cotizacion.objects.order_by('id_personalizado').last()
+    if not last_cotizacion:
+        return '0001'
+    last_id = last_cotizacion.id_personalizado
+    new_id = int(last_id) + 1
+    return str(new_id).zfill(4)
 
 # VISTA DE COTIZACIONES
 def cotizaciones_list(request):
@@ -40,7 +45,7 @@ def cotizaciones_list(request):
     }
     return render(request, "accounts/cotizaciones/cotizaciones.html", context)
 
-
+# VISTA PARA OBTENER INFORMACION DEL CLIENTE HE IMPRMIR EN FORMULARIO
 def obtener_datos_cliente(request, cliente_id):
     persona = Persona.objects.filter(id=cliente_id).select_related(
         'informacion_contacto', 'empresa').first()
@@ -58,11 +63,28 @@ def obtener_datos_cliente(request, cliente_id):
     else:
         return JsonResponse({'error': 'Cliente no encontrado'}, status=404)
 
+# VISTA PARA OBTENER INFORMACION DEL SERVICIO HE IMPRMIR EN FORMULARIO
+def obtener_datos_servicio(request, servicio_id):
+    servicio = Servicio.objects.filter(id=servicio_id).first()
+    if servicio:
+        data = {
+            'nombre': servicio.nombre_servicio,
+            'descripcion': servicio.descripcion,
+            'precio': servicio.precio_sugerido,
+            # Asegúrate de manejar relaciones nulas
+            'metodo': servicio.metodo.metodo if servicio.metodo else 'No disponible',
+            # Agrega otros campos que necesites
+        }
+        return JsonResponse(data)
+    else:
+        return JsonResponse({'error': 'Servicio no encontrado'}, status=404)
+
 # AGREGAR NUEVA COTIZACION
 def cotizacion_form(request):
     if request.method == 'POST':
         cotizacion_form = CotizacionForm(request.POST)
         concepto_formset = ConceptoFormSet(request.POST)
+        
 
         if cotizacion_form.is_valid() and concepto_formset.is_valid():
             try:
@@ -96,6 +118,23 @@ def cotizacion_form(request):
     else:
         cotizacion_form = CotizacionForm()
         concepto_formset = ConceptoFormSet()
+        
+        # Inicializar formularios para crear prospectos
+        titulos = Titulo.objects.all()
+        empresas = Empresa.objects.all()
+        persona_form = PersonaForm()
+        empresa_form = EmpresaForm()
+        prospecto_from = ProspectoForm()
+        
+        context={
+            'cotizaxion_form': cotizacion_form,
+            'concepto_formset': concepto_formset,
+            'titulos': titulos,
+            'empresas': empresas,
+            'persona_form': persona_form,
+            'empresa_form': empresa_form,
+            'prospecto_form': prospecto_from
+        }
 
     return render(request, 'accounts/cotizaciones/cotizaciones_registro.html', {
         'cotizacion_form': cotizacion_form,
@@ -131,7 +170,7 @@ def cotizaciones_prospecto_create(request):
                 else:
                     messages.error(
                         request, 'Por favor, corrige los errores en el formulario de empresa.')
-                    return render(request, 'accounts/prospectos/prospectos.html', {
+                    return render(request, 'accounts/cotizaciones/cotizaciones_preregistro.html', {
                         'persona_form': persona_form,
                         'empresa_form': empresa_form,
                         'direccion_form': direccion_form,
@@ -143,7 +182,7 @@ def cotizaciones_prospecto_create(request):
             else:
                 messages.error(
                     request, 'Por favor, selecciona o crea una empresa.')
-                return render(request, 'accounts/prospectos/prospectos.html', {
+                return render(request, 'accounts/cotizaciones/cotizaciones_preregistro.html', {
                     'persona_form': persona_form,
                     'empresa_form': empresa_form,
                     'empresas': Empresa.objects.all(),
@@ -161,17 +200,23 @@ def cotizaciones_prospecto_create(request):
             prospecto.save()
 
             messages.success(request, 'Prospecto creado.')
-            cotizacion_form = CotizacionForm(
-                initial={'persona': persona, 'metodo_pago': prospecto.persona.empresa.moneda})
+            
+            cotizacion_form = CotizacionForm(initial={'persona': persona, 'metodo_pago': prospecto.persona.empresa.moneda})
+            
+            
             concepto_formset = ConceptoFormSet()
             return render(request, 'accounts/cotizaciones/cotizaciones_registro.html', {
                 'cotizacion_form': cotizacion_form,
                 'concepto_formset': concepto_formset,
+                'empresa':empresa.nombre_empresa,
+                'persona':persona,
+                'metodo_pago': empresa.moneda,
+                'informacionContacto': informacion_contacto,
+                'rfc': empresa.rfc
             })
 
         else:
-            messages.error(
-                request, 'Por favor, corrige los errores en el formulario.')
+            messages.error(request, 'Por favor, corrige los errores en el formulario.')
     else:
         persona_form = PersonaForm()
         empresa_form = EmpresaForm()
@@ -185,7 +230,8 @@ def cotizaciones_prospecto_create(request):
         'empresas': Empresa.objects.all(),
         'titulos': titulos,
     }
-    return render(request, 'accounts/prospectos/prospectos.html', context)
+    return render(request, 'accounts/cotizaciones/cotizaciones_preregistro.html', context)
+
 
 # INTERFAZ DE DETALLES DE CADA COTIZACION
 def cotizacion_detalle(request, pk):
@@ -209,48 +255,49 @@ def cotizacion_delete(request, pk):
         return redirect('cotizaciones_list')
     return render(request, 'accounts/cotizaciones/eliminar_colitazion.html', {'cotizacion': cotizacion})
 
-# VISTA PARA EDITAR COTIZACION
+# Vista para editar cotización
 def cotizacion_edit(request, pk):
-    # Obtiene una instancia del modelo de cotizacion desde pk, si no encuentra devuelve error 404
     cotizacion = get_object_or_404(Cotizacion, id=pk)
-    # print(f"Fecha Solicitada: {cotizacion.fecha_solicitud}, Fecha Caducidad: {cotizacion.fecha_caducidad}")
-    if request.method == 'POST':  # Si el formulario a sido enviado
-        # Crear instancias de los formularios CotizacionChangeForm y ConceptoChangeFormSet, pre-poblados con los datos enviados en la solicitud POST.
-        cotizacion_form = CotizacionChangeForm(
-            request.POST, instance=cotizacion)
-        concepto_formset = ConceptoChangeFormSet(
-            request.POST, instance=cotizacion)
-        # request.post contiene los datos enviados, instance = cotizacion vincula los formularios a la existente de cotizacion
 
-        if cotizacion_form.is_valid() and concepto_formset.is_valid():  # verifica que los formularios sean validos
-            #
+    if request.method == 'POST':
+        cotizacion_form = CotizacionChangeForm(request.POST, instance=cotizacion)
+        concepto_formset = ConceptoFormSet(request.POST, instance=cotizacion)
+
+        if cotizacion_form.is_valid() and concepto_formset.is_valid():
+            # Guardar la instancia de cotización y los conceptos asociados
             cotizacion = cotizacion_form.save()
-            conceptos = concepto_formset.save(commit=False)
-            for concepto in conceptos:
-                concepto.cotizacion = cotizacion
-                concepto.save()
-            for concepto in concepto_formset.deleted_objects:
-                concepto.delete()
+            concepto_formset.save()
 
-            cotizacion.subtotal = sum(
-                [c.cantidad_servicios * c.precio for c in conceptos])
-            cotizacion.iva = cotizacion.subtotal * (cotizacion.tasa_iva / 100)
-            cotizacion.total = cotizacion.subtotal + cotizacion.iva
-            cotizacion.save()
-            messages.info(request, 'Editando cotización.')
-            # Redirigir al usuario a la vista de detalles de la cotización después de guardar los cambios.
+            # Recalcular subtotal, iva y total de todos los conceptos relacionados con esta cotización
+            with transaction.atomic():
+                conceptos = cotizacion.conceptos.all()
+                subtotal = sum([concepto.cantidad_servicios * concepto.precio for concepto in conceptos])
+                iva = subtotal * (cotizacion.tasa_iva / 100)
+                total = subtotal + iva
+
+                # Actualizar los valores en la instancia de cotización
+                cotizacion.subtotal = subtotal
+                cotizacion.iva = iva
+                cotizacion.total = total
+                cotizacion.save()
+
+            messages.info(request, 'Cotización editada exitosamente.')
             return redirect('cotizacion_detalle', pk=cotizacion.id)
     else:
-        # Inicializar los formularios con los datos actuales de la cotización y sus conceptos cuando la solicitud no es POST (es decir, en GET).
         cotizacion_form = CotizacionChangeForm(instance=cotizacion)
-        concepto_formset = ConceptoChangeFormSet(instance=cotizacion)
-    # cotizaciones_editar.html con los formularios de cotización y conceptos.
-    return render(request, 'accounts/cotizaciones/cotizaciones_editar.html', {
+        concepto_formset = ConceptoFormSet(instance=cotizacion)
+        
+    context = {
         'cotizacion_form': cotizacion_form,
         'concepto_formset': concepto_formset,
         'cotizacion': cotizacion,
-        'edit': True
-    })
+        'persona': cotizacion.persona.nombre,
+        'empresa': cotizacion.persona.empresa.nombre_empresa,
+        'rfc': cotizacion.persona.empresa.rfc,
+        'informacionContacto': cotizacion.persona.informacion_contacto,
+        'edit': True  # Esta bandera se puede usar para ajustar la interfaz según si es edición o creación
+    }
+    return render(request, 'accounts/cotizaciones/cotizaciones_editar.html', context)
 
 # VISTA PARA DUPLICAR COTIZACION
 def cotizacion_duplicar(request, pk):
@@ -296,14 +343,6 @@ def cotizacion_duplicar(request, pk):
         'concepto_formset': formset
     })
 
-def generate_new_id_personalizado():
-    last_cotizacion = Cotizacion.objects.order_by('id_personalizado').last()
-    if not last_cotizacion:
-        return '0001'
-    last_id = last_cotizacion.id_personalizado
-    new_id = int(last_id) + 1
-    return str(new_id).zfill(4)
-
 # VISTA PARA GENERAR ARCHIVO PDF
 def cotizacion_pdf(request, pk):
     cotizacion = get_object_or_404(Cotizacion, id=pk)
@@ -342,31 +381,3 @@ def cotizacion_pdf(request, pk):
         cotizacion.id_personalizado}.pdf"'
     return response
 
-# VISTA PARA MODIFICAR NUESTRA INFORMACION DE FORMATO
-def terminos_avisos(request):
-    formato = get_object_or_404(Formato, id=1)
-    if request.method == 'POST':
-        form = TerminosForm(request.POST, instance=formato)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Terminos actualizados.')
-            # Redirige a la vista deseada después de guardar
-            return redirect('home')
-    else:
-        form = TerminosForm(instance=formato)
-    return render(request, 'accounts/organizacion/terminos.html', {'form': form})
-
-def obtener_datos_servicio(request, servicio_id):
-    servicio = Servicio.objects.filter(id=servicio_id).first()
-    if servicio:
-        data = {
-            'nombre': servicio.nombre_servicio,
-            'descripcion': servicio.descripcion,
-            'precio': servicio.precio_sugerido,
-            # Asegúrate de manejar relaciones nulas
-            'metodo': servicio.metodo.metodo if servicio.metodo else 'No disponible',
-            # Agrega otros campos que necesites
-        }
-        return JsonResponse(data)
-    else:
-        return JsonResponse({'error': 'Servicio no encontrado'}, status=404)
