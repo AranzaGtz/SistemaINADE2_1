bb5cf69e546d12229902c3a9a500ec243e12fc5e
AranzaGtz <aranza.gutierrezm.ok@gmail.com>
2024-08-15 16:14:43 -0700

    Personalizando logo

--- accounts/vistas/views_home.py
+++ accounts/vistas/views_home.py
@@ -1,10 +1,11 @@
 # VISTA PARA DIRIGIR A INTERFAZ DE CLIENTES
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from accounts.models import Cotizacion, Notificacion
+from accounts.models import Cotizacion, Notificacion, Organizacion
 
 @login_required
 def home(request):
+    organizacion = Organizacion.objects.first()  # Asume que solo hay una organización
     # Obtener todas las notificaciones del usuario
     notificaciones = request.user.notificacion_set.all()
     
@@ -20,4 +21,6 @@ def home(request):
         'notificaciones_no_leidas': notificaciones_no_leidas,
         'total_cotizaciones': total_cotizaciones,
         'cotizaciones_aceptadas': cotizaciones_aceptadas,
+        'organizacion': organizacion,
     })
+    
