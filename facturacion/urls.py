bb05b110d59194255eec7f938f0fd07283b725c5
AranzaGtz <aranza.gutierrezm.ok@gmail.com>
2024-08-19 16:32:36 -0700

    Actualizacion de facturas

---
+++ facturacion/urls.py
@@ -0,0 +1,7 @@
+# facturacion/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('crear_factura/<id_personalizado>', views.crear_factura, name='crear_factura'),
+]
