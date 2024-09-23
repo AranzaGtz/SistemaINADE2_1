be4ea5a3e11d1fa6f9a0983c8d55908744de1282
AranzaGtz <aranza.gutierrezm.ok@gmail.com>
2024-09-23 10:58:45 -0700

    agregando factura de API a BD

--- facturacion/forms.py
+++ facturacion/forms.py
@@ -66,10 +66,10 @@ class FacturaForm(forms.ModelForm):
 class FacturaEncabezadoForm(forms.ModelForm):
     class Meta:
         model = Factura
-        fields = ['tipo_moneda','orden_compra', 'uso_cfdi', 'forma_pago','metodo_pago']
+        fields = ['tipo_moneda','OrderNumber', 'uso_cfdi', 'forma_pago','metodo_pago']
         widgets = {
             'tipo_moneda': forms.Select(attrs={'class': 'form-control ', 'placeholder': 'Seleccione tipo moneda'}),
-            'orden_compra': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'ID de orden de compra'}),
+            'OrderNumber': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'ID de orden de compra'}),
             'uso_cfdi': forms.Select(attrs={'class': 'form-control'}),
             'forma_pago': forms.Select(attrs={'class': 'form-control'}),
             'metodo_pago': forms.Select(attrs={'class': 'form-control'}),
